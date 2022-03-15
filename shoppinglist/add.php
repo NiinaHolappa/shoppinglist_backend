247d0c73bad77c3f532533a22b9a3850232ba201
Holappa NiinaE <niina.p.puurunen@gmail.com>
2022-03-15 20:04:47 +0200

    tehty muutoksia toimivuuteen

--- shoppinglist/add.php
+++ shoppinglist/add.php
@@ -4,16 +4,18 @@ require_once 'inc/functions.php';
 
 $input = json_decode(file_get_contents('php://input'));
 $description = filter_var($input->description,FILTER_SANITIZE_SPECIAL_CHARS);
+$amount = filter_var($input->amount,FILTER_SANITIZE_NUMBER_INT);
 
 try {
 $db = openDB();
 
-$query = $db->prepare('insert into item(description) values (:description)');
+$query = $db->prepare('insert into item(description, amount) values (:description, :amount)');
 $query->bindValue(':description', $description,PDO::PARAM_STR);
+$query->bindValue(':amount', $amount,PDO::PARAM_INT);
 $query->execute();
 
 header('HTTP/1.1 200 OK');
-$data = array('id' => $db->lastInsertId(),'description' => $description);
+$data = array('id' => $db->lastInsertId(),'description' => $description, 'amount' => $amount);
 print json_encode($data);
 } catch (PDOException $pdoex) {
     returnError($pdoex);
